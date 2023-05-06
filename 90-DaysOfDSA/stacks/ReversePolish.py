46b80a0b2793ea425f451eb32ed8ed918671825e
kumaramit123 <amit.kumar@acem.edu.in>
2023-05-06 12:26:39 +0530

    day 2

---
+++ 90-DaysOfDSA/stacks/ReversePolish.py
@@ -0,0 +1,51 @@
+tokens = ["2","1","+","3","*"]
+
+
+# def evalRPN(tokens):
+#     res = []
+
+#     for i in tokens:
+#         if i == "-" or i == "/" :
+#             a,b = res.pop(), res.pop()
+#             if i == "-":
+#                 res.append(b-a)
+#             else:
+#                 res.append(int(b/a))
+#         elif i == "+" or i=="*":
+#             a,b = res.pop(), res.pop()
+
+#             if i == "+":
+#                 res.append(a + b)
+#             else:
+#                 res.append(a*b)
+
+      
+#         else:
+#             res.append(int(i))
+
+#     return res[0]
+
+
+# print(evalRPN(tokens))
+
+def evalRPN(tokens):
+    res = []
+
+    for i in tokens:
+        if i == "+" :
+            res.append(res.pop() + res.pop())
+        elif i == "-":
+            a,b = res.pop(), res.pop()
+            res.append(b-a)
+        elif i == "*":
+            res.append(res.pop()+ res.pop())
+        elif i == "/":
+            res.append(int(b/a))
+        else:
+            res.append(int(i))
+    
+            
+
+    return res[0]
+
+print(evalRPN(tokens))

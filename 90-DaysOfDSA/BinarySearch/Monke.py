32d0b852c4ff8586f214ee595525dbf0b5cc59e6
zuggernautt <amitak0707@gmail.com>
2023-05-11 01:18:05 +0530

    day 5

--- 90-DaysOfDSA/BinarySearch/Monke.py
+++ 90-DaysOfDSA/BinarySearch/Monke.py
@@ -1,2 +1,22 @@
+piles,h=[312884470], 312884469
+import math
+
 def minEatingSpeed(piles, h):
-    
+    l, r = 1, max(piles)
+    res = max(piles)
+    while l<=r:
+        time = 0
+        k = (l+r)//2
+        for p in piles:
+            time += math.ceil(p / k)
+        if time <= h:
+            
+            res = min(res, k)
+            r= k-1
+        else:
+            l=k+1
+
+    return res
+            
+
+print(minEatingSpeed(piles, h))

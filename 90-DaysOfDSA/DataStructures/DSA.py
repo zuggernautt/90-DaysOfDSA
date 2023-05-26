4d9642f102a5d60a4ce743c27eafc7208ceaf9e4
zuggernautt <amitak0707@gmail.com>
2023-05-26 23:12:25 +0530

    practice

---
+++ 90-DaysOfDSA/DataStructures/DSA.py
@@ -0,0 +1,15 @@
+#array using list
+x= [1,2,3,4,5]
+
+x.append(6)
+x.append(13)
+x.append(11)
+x.pop()
+#print(x[-1])
+
+#dictionary
+y= {"a":"1", "b":"2", "c":3, 6:"f"}
+#print(y.get(6))
+
+z = (1,2,3,4,5)
+z.append(8)

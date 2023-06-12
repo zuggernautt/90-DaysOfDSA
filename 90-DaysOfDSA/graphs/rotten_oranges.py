eb7a0f4119447e73e39007524ce80b0b58ca2d31
zuggernautt <amitak0707@gmail.com>
2023-06-12 12:52:40 +0530

    graph algo

---
+++ 90-DaysOfDSA/graphs/rotten_oranges.py
@@ -0,0 +1,28 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+
+from collections import deque
+
+
+def bfs(graph, start_node):
+    visited  = set()
+    queue = deque(start_node)
+    while queue:
+        node = queue.popleft()
+        visited.add(node)
+        neighbours = graph(node)
+
+        for neighbour in neighbours:
+            if neighbour not in queue:
+                queue.extend(neighbour)
+                visited.add(neighbour)
+
+print(bfs(graph,start_node='A'))
+

eaa266ef01bf13ea79e5be47aad580a74aee8760
zuggernautt <amitak0707@gmail.com>
2023-06-16 18:12:51 +0530

    traversals

---
+++ 90-DaysOfDSA/Trees/max_Depth.py
@@ -0,0 +1,46 @@
+# Recursive DFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root):
+        if not root :
+            return 0
+        left_max = self.maxDepth(root.left)
+        right_max = self.maxDepth(root.right)
+        return  1+ max(left_max, right_max)
+    
+
+
+#bfs
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root):
+        if not root :
+            return 0
+        level = 0 
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level+=1
+
+        return level
+    
+#iterative DFS
+
+

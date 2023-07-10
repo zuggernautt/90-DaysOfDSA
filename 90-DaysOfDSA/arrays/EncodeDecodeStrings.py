8f9f99d953d3bdc4ce28d24559927a4e15f90461
zuggernautt <amitak0707@gmail.com>
2023-07-10 09:20:22 +0530

    .

--- 90-DaysOfDSA/arrays/EncodeDecodeStrings.py
+++ 90-DaysOfDSA/arrays/EncodeDecodeStrings.py
@@ -1,4 +1,29 @@
-def encode:
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        y = ""
+        for x in strs:
+            y+= str(len(x))+'|'+x
+        return y
 
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        res = []
+        i=0
+        while i<len(str):
+            j=i
+            while str[j]!='|':
+                j+=1
+            length = int(str[i:j])
+            res.append(str[j+1:j+1+length])
+            i=j+1+length
+        return res
 
-def decode:

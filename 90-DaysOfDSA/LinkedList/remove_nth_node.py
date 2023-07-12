56f486dd3d268a68fbab256a4c332969aebc687b
zuggernautt <amitak0707@gmail.com>
2023-07-12 23:52:37 +0530

    linked_ist

--- 90-DaysOfDSA/LinkedList/remove_nth_node.py
+++ 90-DaysOfDSA/LinkedList/remove_nth_node.py
@@ -7,3 +7,5 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         temp=head
         head.next
+
+#  brute for to reverse the linked list and remove kth term from start

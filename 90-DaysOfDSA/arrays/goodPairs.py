d91935b3539592507347e40917ea64757d7503ad
zuggernautt <amitak0707@gmail.com>
2023-10-03 23:41:35 +0530

    POTD

---
+++ 90-DaysOfDSA/arrays/goodPairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        res = 0
+        for i in count.values():
+            res+=((i)*(i-1))/2
+        return int(res)
+
